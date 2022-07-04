b5ebb919c58a152b8411dbda5efeb34e18d92786
Sourav Das <94233240+Dassourav07@users.noreply.github.com>
2022-07-04 20:23:37 +0530

    Update projects.js

--- src/components/Project/projects.js
+++ src/components/Project/projects.js
@@ -9,7 +9,7 @@ export let projects = [
     image:  "https://user-images.githubusercontent.com/94233240/177165606-a30eca8a-5171-474b-8aec-8b238f001aa0.jpg",
     title: "Cryptocurrency tracker application using React.js",
     info: "cryptocurrency tracker application using React.js, live marketcap and valuation of different cryptocurrencys",
-    github: "https://github.com/Dassourav07/canvas_social",
+    github: "https://github.com/Dassourav07/Crypto_Coin",
     techstack: ["React.js", "Chart.js", "MaterialUI", "JavaScript", "node.js", "gecko coin api"],
   },
  {
